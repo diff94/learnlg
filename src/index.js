715fda0952077d02da4493a4b960450279666821
diff94 <62204022+diff94@users.noreply.github.com>
2022-11-30 22:58:29 +0800

    Add slash to basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="learnlg">
+    <BrowserRouter basename="/learnlg">
       <App />
     </BrowserRouter>
   </React.StrictMode>
